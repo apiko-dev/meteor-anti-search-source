253c464c6de3a1d023f302a4fd1777a2e0cdb712
alonoslav <vadym.vorobel@gmail.com>
2016-12-20 12:35:54 +0200

    * Add check for allow rule existing * Refactoring

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   name: 'jss:anti-search-source',
-  version: '1.4.0',
+  version: '1.4.1',
   summary: 'Flexible search in collections based on publish/subscribe',
   git: 'https://github.com/JSSolutions/meteor-anti-search-source',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
+// eslint-disable-next-line func-names, prefer-arrow-callback
 Package.onUse(function (api) {
   api.versionsFrom('1.3');
   api.use('templating');
